23200b496b1d69620993a469b40c99cafddff067
John Edward Quintero Lozano <john.quinterol@universitasxxi.com>
2025-07-07 18:31:58 -0500

    Upload files nodejsapp

---
+++ DSL/nodejsDockerDSL.groovy
@@ -0,0 +1,44 @@
+job('Aplicacion Node.js Docker DSL') {
+    description('Aplicación Node JS Docker DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/johnrip84/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('johnrip84')
+            node / gitConfigEmail('johnrip84@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('johnrip84/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
